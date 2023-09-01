83bf98173cadb2f1faaafaaedf27437490b24b7b
Arifin Kusuma <finkusuma.dev@gmail.com>
2023-09-01 14:57:38 +0700

    added morgan to log request, and dotenv to set env vars

--- natours/app.js
+++ natours/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
+const morgan = require('morgan');
 
 const app = express();
+
+const logger = morgan('dev');
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -10,6 +13,10 @@ app.use(express.static(`${__dirname}/public`));
 // app.get('/', (req, res) => {
 //   res.json({ success: true });
 // });
+//console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger);
+}
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,13 @@
+const dotenv = require('dotenv');
+
+// dotenv.config(); //load .env (default env file)
+dotenv.config({ path: './config.env' }); ///load custom env file
 const app = require('./app');
 
-const port = 3000;
+// console.log('process.env', process.env);
+// console.log('process.env.PORT', process.env.PORT);
+
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`server listening port ${port}`);
 });
