3ee2619778c6b004f4eb661bfa7fd893e44794e1
Arifin Kusuma <finkusuma.dev@gmail.com>
2023-09-03 07:09:22 +0700

    describe tour schema and add new tour

--- natours/server.js
+++ natours/server.js
@@ -7,8 +7,52 @@ dotenv.config({ path: './config.env' }); ///load custom env file
 // console.log('database', process.env.DATABASE);
 mongoose
   .connect(process.env.DATABASE, {})
-  .then((con) => console.log('Db connected', con.connections))
+  .then(() => console.log('Db connected'))
   .catch((err) => console.log('connected failed', err));
+
+const tourSchema = mongoose.Schema({
+  // id: Number,
+  name: {
+    type: String,
+    required: [true, 'a tour must have a name'],
+    unique: true,
+  },
+  // duration: Number,
+  // maxGrupSize: Number,
+  // difficulty: String,
+  // ratingsAverage: Number,
+  // ratingsQuantity: Number,
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'a tour must have a price'],
+  },
+  // summary: String,
+  // description: String,
+  // imageCover: String,
+  // images: String,
+  // startDates: String,
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+Tour.findOne({ name: 'The Forest Adventurer' }).then((doc) => {
+  if (doc) {
+    console.log('Found the tour', doc);
+  } else {
+    const tour = new Tour({
+      name: 'The Forest Adventurer',
+      price: 200,
+    });
+    tour
+      .save()
+      .then((newDoc) => console.log('A new tour created:', newDoc))
+      .catch((err) => console.log('Save a new tour failed:', err));
+  }
+});
 
 const app = require('./app');
 
