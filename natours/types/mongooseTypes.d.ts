96267bb1212a8f32548bbbff7dee5a7c8a793e41
Arifin Kusuma <finkusuma.dev@gmail.com>
2023-09-07 14:42:23 +0700

    added ModelTypes

--- natours/types/mongooseTypes.d.ts
+++ natours/types/mongooseTypes.d.ts
@@ -1,6 +1,25 @@
 import { Query, Document, Types } from 'mongoose';
-export type QueryType<T> = Query<(Document<unknown, {}, T> & T & {
-    _id: Types.ObjectId;
-})[], Document<unknown, {}, T> & T & {
-    _id: Types.ObjectId;
-}, {}, T>;
+export type QueryType<T> = Query<
+  (Document<unknown, {}, T> &
+    T & {
+      _id: Types.ObjectId;
+    })[],
+  Document<unknown, {}, T> &
+    T & {
+      _id: Types.ObjectId;
+    },
+  {},
+  T
+>;
+
+export type ModelType<T> = Model<
+  T,
+  {},
+  {},
+  {},
+  Document<unknown, {}, T> &
+    T & {
+      _id: Types.ObjectId;
+    },
+  any
+>;
