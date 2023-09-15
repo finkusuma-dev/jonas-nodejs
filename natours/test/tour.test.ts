63fac2b0d97520dc172fce27f712a082d6eaf4c0
Arifin Kusuma <finkusuma.dev@gmail.com>
2023-09-15 22:01:13 +0700

    test: new add, get, update, delete a tour

--- natours/test/tour.test.ts
+++ natours/test/tour.test.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import * as dbUtils from '../utils/dbUtils';
 import mongoose from 'mongoose';
 import app from '../app';
@@ -71,7 +71,6 @@ beforeAll(() => {
     // console.log('jsonFile', jsonFile);
     await dbUtils.clearData();
     // await dbUtils.importFile(jsonFile);
-    await dbUtils.importData(dataTours);
 
     // console.log('process.env', process.env);
     // console.log('process.env.PORT', process.env.PORT);
@@ -100,14 +99,123 @@ afterAll(() => {
   })();
 });
 
-describe('Testing Tours Route', () => {
+describe('Testing Tours CRUD', () => {
   // test('GET all 9 data', async () => {
   //   const res = await axios.get(URL + '/tours?limit=20');
   //   expect(res.status).toBe(200);
   //   expect(res.data).toHaveProperty('results');
   //   expect(res.data.results).toBe(2);
   // });
+
+  describe('Testing add, get a tour, update, and delete', () => {
+    let idForUpdateDelete = 0;
+
+    test('Add new tour. Has slug', async () => {
+      const res = await axios.post(URL + '/tours', {
+        name: 'aaaaaaaaaa',
+        duration: 4,
+        maxGroupSize: 5,
+        difficulty: 'easy',
+        price: 700,
+        priceDiscount: 699,
+        summary: '-',
+      });
+      idForUpdateDelete = res.data.data.tour['_id'];
+      expect(res.data.results).toBe(1);
+      expect(res.data.data.tour).toHaveProperty('slug');
+    });
+
+    test('Get a tour', async () => {
+      const res = await axios.get(URL + '/tours/' + idForUpdateDelete);
+      expect(res.data.data.tour.name).toMatch(/^aaaaaaaaaa$/);
+    });
+
+    test('FAILED to add new tour (name<10 & name>30) ', async () => {
+      try {
+        await axios.post(URL + '/tours', {
+          name: '123456789',
+          duration: 4,
+          maxGroupSize: 5,
+          difficulty: 'easy',
+          price: 700,
+          summary: '-',
+        });
+        // console.log('axios res', res);
+      } catch (err: any) {
+        // console.log('axios error:', err);
+        // console.log('axios error.message:', (err as AxiosError).message);
+        expect((err as AxiosError).message).toMatch('400');
+      }
+      try {
+        await axios.post(URL + '/tours', {
+          name: '123456789 123456789 123456789 1',
+          duration: 4,
+          maxGroupSize: 5,
+          difficulty: 'easy',
+          price: 700,
+          summary: '-',
+        });
+      } catch (err) {
+        expect((err as AxiosError).message).toMatch('400');
+      }
+    });
+    test('FAILED to add new tour (priceDiscount >= price)', async () => {
+      try {
+        const res = await axios.post(URL + '/tours', {
+          name: '123456789',
+          duration: 4,
+          maxGroupSize: 5,
+          difficulty: 'easy',
+          price: 500,
+          priceDiscount: 500,
+          summary: '-',
+        } as ITour);
+      } catch (err) {
+        // expect((err as AxiosError).message).toMatch('400');
+        expect((err as AxiosError).response!.status).toBe(400);
+      }
+    });
+
+    test('Update tour', async () => {
+      const res = await axios.patch(URL + `/tours/${idForUpdateDelete}`, {
+        difficulty: 'difficult',
+        price: 500,
+      } as ITour);
+      // expect(res.data.results).toBe(2);
+      // const res2 = await axios.get(URL + '/tours');
+      // console.log('res.data.data',res.data.data)
+      expect(res.data.data.tour.difficulty).toMatch('difficult');
+      expect(res.data.data.tour.price).toBe(500);
+    });
+    test('Delete tour', async () => {
+      const res = await axios.delete(URL + `/tours/${idForUpdateDelete}`);
+      const res2 = await axios.get(URL + '/tours');
+      expect(res2.data.results).toBe(0);
+    });
+    test('Add new secret tour', async () => {
+      const res = await axios.post(URL + '/tours', {
+        name: 'cccccccccc',
+        duration: 5,
+        maxGroupSize: 5,
+        difficulty: 'medium',
+        price: 300,
+        secret: true,
+        summary: '-',
+      } as ITour);
+      expect(res.data.results).toBe(1);
+      const res2 = await axios.get(URL + '/tours');
+      expect(res2.data.results).toBe(0);
+    });
+  });
+
   describe('Testing GET Tours with Querystring', () => {
+    beforeAll(() => {
+      return (async function () {
+        await dbUtils.clearData();
+        return dbUtils.importData(dataTours);
+      })();
+    });
+
     describe('Pagination', () => {
       test('First page has 5 results', async () => {
         const res = await axios.get(URL + '/tours');
