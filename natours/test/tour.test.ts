f8a40ba02bcb51488a42b6d58629aac2638e85ec
Arifin Kusuma <finkusuma.dev@gmail.com>
2023-09-15 17:53:16 +0700

    test: add filtering & sorting, use custom data in test

--- natours/test/tour.test.ts
+++ natours/test/tour.test.ts
@@ -4,8 +4,60 @@ import * as dbUtils from '../utils/dbUtils';
 import mongoose from 'mongoose';
 import app from '../app';
 import { Server } from 'http';
+import { ITour } from '../models/tourModel';
 // import { ITour } from '../models/tourModel';/
 // import type Server from '@types/express';
+
+const dataTours = [
+  {
+    name: 'aaaaaaaaaa',
+    duration: 4,
+    maxGroupSize: 5,
+    difficulty: 'easy',
+    price: 700,
+    summary: '-',
+  },
+  {
+    name: 'dddddddddd',
+    duration: 6,
+    maxGroupSize: 5,
+    difficulty: 'difficult',
+    price: 100,
+    summary: '-',
+  },
+  {
+    name: 'cccccccccc',
+    duration: 5,
+    maxGroupSize: 5,
+    difficulty: 'medium',
+    price: 300,
+    summary: '-',
+  },
+  {
+    name: 'ffffffffff',
+    duration: 1,
+    maxGroupSize: 5,
+    difficulty: 'difficult',
+    price: 100,
+    summary: '-',
+  },
+  {
+    name: 'eeeeeeeeee',
+    duration: 3,
+    maxGroupSize: 5,
+    difficulty: 'easy',
+    price: 400,
+    summary: '-',
+  },
+  {
+    name: 'bbbbbbbbbb',
+    duration: 2,
+    maxGroupSize: 5,
+    difficulty: 'difficult',
+    price: 200,
+    summary: '-',
+  },
+];
 
 const HTTP_PORT = 3100;
 const URL = `http://127.0.0.1:${HTTP_PORT}/api/v1`;
@@ -14,10 +66,11 @@ let serverHandle: Server<any>;
 beforeAll(() => {
   async function init() {
     await dbUtils.connectDb(true);
-    const jsonFile = `${__dirname}/../dev-data/data/tours.json`;
-    console.log('jsonFile', jsonFile);
+    // const jsonFile = `${__dirname}/../dev-data/data/tours.json`;
+    // console.log('jsonFile', jsonFile);
     await dbUtils.clearData();
-    await dbUtils.importFile(jsonFile);
+    // await dbUtils.importFile(jsonFile);
+    await dbUtils.importData(dataTours);
 
     // console.log('process.env', process.env);
     // console.log('process.env.PORT', process.env.PORT);
@@ -47,44 +100,79 @@ afterAll(() => {
 });
 
 describe('Testing Tours', () => {
-  test('GET all 9 data', async () => {
-    const res = await axios.get(URL + '/tours?limit=20');
-    expect(res.status).toBe(200);
-    expect(res.data).toHaveProperty('results');
-    expect(res.data.results).toBe(9);
+  // test('GET all 9 data', async () => {
+  //   const res = await axios.get(URL + '/tours?limit=20');
+  //   expect(res.status).toBe(200);
+  //   expect(res.data).toHaveProperty('results');
+  //   expect(res.data.results).toBe(2);
+  // });
+
+  describe('Pagination', () => {
+    test('First page 5 results', async () => {
+      const res = await axios.get(URL + '/tours');
+      // expect(res.data).toHaveProperty('results');
+      expect(res.data.results).toBe(5);
+    });
+    test('Second page 1 results', async () => {
+      const res = await axios.get(URL + '/tours?page=2');
+      // expect(res.data).toHaveProperty('results');
+      expect(res.data.results).toBe(1);
+    });
   });
-  test('GET with Pagination', async () => {
-    const res = await axios.get(URL + '/tours');
-    expect(res.data).toHaveProperty('results');
-    expect(res.data.results).toBe(5);
-    const res2 = await axios.get(URL + '/tours?page=2');
-    expect(res2.data).toHaveProperty('results');
-    expect(res2.data.results).toBe(4);
+
+  describe('Filtering', () => {
+    test('Difficulty=easy', async () => {
+      const res = await axios.get(URL + '/tours?difficulty=easy');
+      expect(res.data.results).toBe(2);
+    });
+    test('Price=gt:100,lt:700', async () => {
+      const res = await axios.get(URL + '/tours?price=gt:100,lt:700');
+      expect(res.data.results).toBe(3);
+    });
   });
-  test('GET with querystring: fields=name,price', async () => {
-    const res = await axios.get(URL + '/tours?fields=name,price&limit=1');
-    expect(res.data.data).toHaveProperty('tours');
-    expect(res.data).toHaveProperty('results');
-    expect(res.data.results).toBe(1);
-    expect(res.data.data.tours[0]).toHaveProperty('name');
-    expect(res.data.data.tours[0]).toHaveProperty('price');
+
+  describe('Select Fields', () => {
+    test('Fields=name,price', async () => {
+      const res = await axios.get(URL + '/tours?fields=name,price&limit=1');
+
+      expect(res.data.data.tours[0]).toHaveProperty('name');
+      expect(res.data.data.tours[0]).toHaveProperty('price');
+    });
+    test('Fields=-name,-price', async () => {
+      const res = await axios.get(URL + '/tours?fields=-name,-price&limit=1');
+
+      expect(res.data.data.tours[0]).not.toHaveProperty('name');
+      expect(res.data.data.tours[0]).not.toHaveProperty('price');
+    });
   });
-  test('GET with querystring: fields=-name,-price', async () => {
-    const res = await axios.get(URL + '/tours?fields=-name,-price&limit=1');
-    expect(res.data.data).toHaveProperty('tours');
-    expect(res.data).toHaveProperty('results');
-    expect(res.data.results).toBe(1);
-    expect(res.data.data.tours[0]).not.toHaveProperty('name');
-    expect(res.data.data.tours[0]).not.toHaveProperty('price');
-  });
-  test('GET with querystring: fields=name&sort=name', async () => {
-    const res = await axios.get(URL + '/tours?fields=name&sort=name');
-    expect(res.data.data).toHaveProperty('tours');
-    expect(res.data).toHaveProperty('results');
-    expect(res.data.results).toBeGreaterThanOrEqual(1);
-    expect(res.data.data.tours[0]).toHaveProperty('name');
-    expect(res.data.data.tours[1]).toHaveProperty('name');
-    // expect(res.data.data.tours[0].name).toHaveProperty('name');
-    ///TODO: need to figure how to make sure data is sorted
+
+  describe('Sorting', () => {
+    /// Only use 3 tours data for sorting test
+    beforeAll(() => {
+      return (async function () {
+        await dbUtils.clearData();
+        let dataSorting = [...dataTours];
+        dataSorting.splice(3, 3);
+        // console.log('dataSorting', dataSorting);
+        await dbUtils.importData(dataSorting);
+      })();
+    });
+
+    test('sort=name', async () => {
+      const res = await axios.get(URL + '/tours?fields=name&sort=name');
+      const sortedName = (res.data.data.tours as Array<ITour>).map(
+        (el) => el.name,
+      );
+      // console.log('sortedName', sortedName);
+      expect(sortedName).toEqual(['aaaaaaaaaa', 'cccccccccc', 'dddddddddd']);
+    });
+    test('sort=-price', async () => {
+      const res = await axios.get(URL + '/tours?fields=name,price&sort=-price');
+      const sortedPrice = (res.data.data.tours as Array<ITour>).map(
+        (el) => el.price,
+      );
+      // console.log('sortedPrice', sortedPrice);
+      expect(sortedPrice).toEqual([700, 300, 100]);
+    });
   });
 });
